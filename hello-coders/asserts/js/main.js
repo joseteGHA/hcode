44e8f27c51fc9d62394fdd1d015463e3b9eb7c61
Josete <josete@geekshubsacademy.com>
2018-06-13 20:16:16 +0200

    primer push

---
+++ hello-coders/asserts/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  console.log("Se carga Jquery");
+
+  $("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+  });
+});
